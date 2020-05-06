98871f49d7534d1adba4d5b0aae9704eacc17f10
Uriah Harston <uharston@gmail.com>
2020-05-06 22:55:21 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,8 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length #the condition for the iteration in the this method
-    block_return_values << yield(collection[i]) #the while loops its result as an argument for yield and it's method 
-    i += 1 
+    block_return_values << yield(collection[i]) #the while loops its result as an argument for yield and it's method
+    i += 1
   end
   if block_return_values.include?(false)
     false
