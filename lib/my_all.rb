db0e66d9d5f86fe7e5714895477ae7d5d6fd8b75
Uriah Harston <uharston@gmail.com>
2020-05-06 22:55:18 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  block_return_values = []
+  while i < collection.length #the condition for the iteration in the this method
+    block_return_values << yield(collection[i]) #the while loops its result as an argument for yield and it's method 
+    i += 1 
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
+
+my_all?([1,2,3]) {|i| i < 2}
